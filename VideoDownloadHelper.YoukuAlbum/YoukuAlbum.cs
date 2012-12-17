73c887ce443a9460d46c24f8a24d8e2587adf7df
huang yunkun <htynkn@163.com>
2012-12-17 19:31:03 +0800

    Fixed #8
    修正优酷专辑
    说实话，新版本的优酷客户端真心不好用

--- VideoDownloadHelper.YoukuAlbum/YoukuAlbum.cs
+++ VideoDownloadHelper.YoukuAlbum/YoukuAlbum.cs
@@ -18,12 +18,12 @@ namespace VideoDownloadHelper.YoukuAlbum
     {
         public int GetVersionNumber()
         {
-            return 1;
+            return 2;
         }
 
         public string GetVersion()
         {
-            return "V1.0";
+            return "V2.0";
         }
 
         public bool isVaild(string url)
@@ -69,7 +69,7 @@ namespace VideoDownloadHelper.YoukuAlbum
         {
             String temp = WebHelper.GetHtmlCodeByWebClientWithGzip(this.Items[index].Url, "utf-8");
             Document doc = NSoupClient.Parse(temp);
-            Element et = doc.Select("div[id=fn_favodownload] a[id=fn_download]").First;
+            Element et = doc.Select("a#fn_pc_download").First;
             String down = et.Attr("_href");
             System.Diagnostics.Process.Start(down);
         }
