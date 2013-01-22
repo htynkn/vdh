aaabe6464f093a294e747f5d881f7d864d553f40
huang yunkun <htynkn@163.com>
2013-01-22 18:03:32 +0800

    修改主程式的版本号

--- VideoDownloadHelper/Properties/AssemblyInfo.cs
+++ VideoDownloadHelper/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("视频批量下载助手")]
 [assembly: AssemblyDescription("批量下载土豆优酷所有视频")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("sdx")]
+[assembly: AssemblyCompany("Huang Yunkun")]
 [assembly: AssemblyProduct("视频批量下载助手")]
-[assembly: AssemblyCopyright("Copyright sdx©  2011-2012")]
+[assembly: AssemblyCopyright("Copyright Huang Yunkun©2011-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
