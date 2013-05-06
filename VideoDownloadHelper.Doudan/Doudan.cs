c8b1ee49212bbc72c898fdf6fda4e80fba10e179
huang yunkun <htynkn@163.com>
2013-05-06 16:58:52 +0800

    添加RestSharp引用

--- VideoDownloadHelper.Doudan/Doudan.cs
+++ VideoDownloadHelper.Doudan/Doudan.cs
@@ -7,6 +7,9 @@ using NSoup.Select;
 using NSoup.Nodes;
 using NSoup;
 using System.Net;
+using System.Text.RegularExpressions;
+using RestSharp;
+using VideoDownloadHelper.Toudan;
 
 
 [assembly: Addin]
@@ -57,22 +60,17 @@ namespace VideoDownloadHelper.Doudan
 
             String temp = WebHelper.GetHtmlCodeByWebClientWithGzip(this.Url, "gbk");
             Document doc = NSoupClient.Parse(temp);
-            Elements items = doc.Select("div[class=pack pack_video_card]");
-            foreach (Element item in items)
-            {
-                Element e1 = item.Select("h1[class=caption] a").First;
-                Element e2 = item.Select("ul[class=info]").First;
-                String title = e1.Attr("title");
-                String url = e1.Attr("href");
-                String time = e2.Select("li").First.OwnText().Substring("时长:".Length);
-                String upOwner = e2.Select("li a").First.Attr("title");
-                String key = url.Substring(url.LastIndexOf("/") + 1);
-                key = key.Remove(key.IndexOf("."));
 
-                BaseItem b = new BaseItem() { Url = "http://www.tudou.com/programs/view/" + key + "/", Name = title, Time = time, Owner = upOwner };
+            Regex r1=new Regex(@"var lid = '\d+';");
+            Match m1=r1.Match(doc.Html());
+            Regex r2 = new Regex(@"\d+");
+            String lid = r2.Match(m1.ToString()).ToString();
+            String baseUrl = "http://www.tudou.com/plcover/coverPage/getIndexItems.html?lid={id}&page=1&pageSize=500";
 
-                this.Items.Add(b);
-            }
+            var request = new RestRequest(baseUrl, RestSharp.Method.GET);
+            request.AddUrlSegment("id",lid);
+            RestClient rc = new RestClient();
+            IRestResponse<Message> message= rc.Execute<Message>(request);
 
             return this.Items;
         }
---
+++ VideoDownloadHelper.Doudan/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VideoDownloadHelper.Toudan
+{
+    class Message
+    {
+        public int count { get; set; }
+        public List<DoudanItem> items { get; set; }
+    }
+}
