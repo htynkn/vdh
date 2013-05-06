2188d90bb0e84211b5c717848070f218bbaca532
huang yunkun <htynkn@163.com>
2013-05-06 21:36:10 +0800

    提升版本

--- VideoDownloadHelper.Doudan/Doudan.cs
+++ VideoDownloadHelper.Doudan/Doudan.cs
@@ -14,7 +14,7 @@ using Newtonsoft.Json;
 
 
 [assembly: Addin]
-[assembly: AddinDependency("VideoDownloadHelper", "1.7")]
+[assembly: AddinDependency("VideoDownloadHelper", "1.8")]
 
 namespace VideoDownloadHelper.Doudan
 {
