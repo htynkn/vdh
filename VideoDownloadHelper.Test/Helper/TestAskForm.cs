42c6a39910658366cf676cd3001ea794d99aa36d
htynkn <htynkn@163.com>
2013-08-10 15:28:48 +0800

    添加askform支持

---
+++ VideoDownloadHelper.Test/Helper/TestAskForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using VideoDownloadHelper.Helper;
+
+namespace VideoDownloadHelper.Test.Helper
+{
+    [TestFixture]
+    public class TestAskForm
+    {
+        [Test]
+        public void TestPost()
+        {
+            Askform.init("78e62f00-4fd5-49f0-ab06-7becd28250e7", 2);
+            Assert.AreEqual("OK", Askform.post("test", "test2"));
+        }
+    }
+}
